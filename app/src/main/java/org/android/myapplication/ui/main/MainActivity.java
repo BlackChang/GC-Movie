7e5ef802c127fd2366748d7c7b6a83e741f8505e
BlackChang <dnjs7651@gmail.com>
2019-05-20 16:41:13 +0900

    UI base structure & splash

---
+++ app/src/main/java/org/android/myapplication/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package org.android.myapplication.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.android.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/org/android/myapplication/ui/splash/SplashActivity.java
@@ -0,0 +1,28 @@
+package org.android.myapplication.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import org.android.myapplication.R;
+import org.android.myapplication.ui.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
